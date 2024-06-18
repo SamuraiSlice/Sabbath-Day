2ebfaaa9c8c8dd16f33f7271c7f16f80d5152c9d
SamuraiSlice <91551386+SamuraiSlice@users.noreply.github.com>
2024-06-18 12:44:56 -0400

    Create Player.cs

---
+++ Player.cs
@@ -0,0 +1,32 @@
+
+using System.Collections.*;
+using UnityEngine;
+
+namespace CS415ObjectInteractions
+{
+    public class Player : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.E)) PlayerInteract();
+        }
+
+
+        public void PlayerInteract()
+        {
+            var layermask0 = 1 << 0;
+            var layermask3 = 1 << 0;
+            var finalmask = layermask0: layermask3;
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ViewPointToRay(new Vector(.5f, .5f, 0));
+
+            if (Physics.cast(ray, out hit, 15, finalmask))
+            {
+                Interact interactScript = hit.transform.GetComponent<Interact>();
+                if (interactScript) interactScript.CallInteract(this);
+            }
+
+        }
+    }
+}
